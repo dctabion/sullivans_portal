ee35dd8db2aa568c328c8f4243f316d3d8daf5f9
Daniel Tabion <dctabion@gmail.com>
2017-10-18 23:26:21 +0800

    added policies page, moved index to schedules page

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET schedule page. */
+router.get('/schedule', function(req, res, next) {
+  res.render('schedule',
+    { venue: 'Sullivan\'s Steakhouse',
+      bandleader: 'Daniel Tabion'
+  });
+});
+
+/* GET policies page. */
+router.get('/policies', function(req, res, next) {
+  res.render('policies',
+    { venue: 'Sullivan\'s Steakhouse',
+      bandleader: 'Daniel Tabion'
+  });
+});
+
 module.exports = router;
